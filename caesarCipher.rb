336ed0792d4c3ec91b8590615f51abb24cbe0d7c
Makar Rusanivsky <mkr.rus83@gmail.com>
2025-07-16 22:51:58 +0300

    Added a script for encrypting and decrypting messages using the Caesar cipher in Ruby

---
+++ caesarCipher.rb
@@ -0,0 +1,38 @@
+# Caesar cipher
+# This program encrypts and decrypts a message using the Caesar cipher.
+# BSD Licensed
+
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+
+# Key for the Caesar cipher
+key = 13
+
+mode = 'decrypt' # Change to 'decrypt' to decrypt the message
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+translated = ''
+
+message.each_char do |symbols|
+  if SYMBOLS.include?(symbols)
+
+    symbolIndex = SYMBOLS.index(symbols)
+
+    if mode == 'encrypt'
+      translatedIndex = symbolIndex + key
+    elsif mode == 'decrypt'
+      translatedIndex = symbolIndex - key
+    end
+    if translatedIndex >= SYMBOLS.length
+      translatedIndex -= SYMBOLS.length
+    elsif translatedIndex < 0
+      translatedIndex += SYMBOLS.length
+    end
+    translated += SYMBOLS[translatedIndex]
+  else
+    translated += symbols  # Add the symbol without change
+  end
+end
+
+puts "#{mode.capitalize}ed message: #{translated}"
+# Output the translated message
